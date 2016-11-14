4d942e2dc8687dcaf30e874e544f8281b3072dc4
JhonatanBarrera <JhonatanBarrera>
2016-11-14 11:50:01 -0500

    Se añade funcion de lectura de datos desde archivo.

--- MetodosDeOrdenamiento/HeapSort/HeapSort.py
+++ MetodosDeOrdenamiento/HeapSort/HeapSort.py
@@ -1,5 +1,10 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
 # Python program for implementation of heap Sort
+import argparse
 from math import floor
+
 
 def HEAPSORT(A):
     heapSize = len(A)
@@ -8,12 +13,12 @@ def HEAPSORT(A):
         A[0],A[i-1] = A[i-1],A[0]
         heapSize = heapSize - 1
         MAXHEAPIFY(A,1,heapSize)
-#end HEAPSORT
+# end HEAPSORT
 
 def BUILDMAXHEAP(A,hS):
     for i in range(floor(hS/2),0,-1):
         MAXHEAPIFY(A,i,hS)
-#end BUILD-MAX-HEAP
+# end BUILD-MAX-HEAP
 
 def MAXHEAPIFY(A,i,hS):
     l = 2*i
@@ -31,7 +36,26 @@ def MAXHEAPIFY(A,i,hS):
         MAXHEAPIFY(A,largest,hS)
 # end MAX-HEAPIFY
 
-arr = [4,10,3,5,1,6,5,4,8,12,54,-743,1,54,32]
-HEAPSORT(arr)
-n = len(arr)
-print ("Sorted array is", arr)
+def READDATAFILE(nums):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", help="Nombre del archivo a procesar.")
+    args = parser.parse_args()
+
+    if args.file:
+        print("El nombre del archivo a procesar es: ", args.file)
+    
+    file = open(args.file)
+    data = file.read()
+    
+    for word in data.split():
+        nums.append(int(word))
+# end READDATAFILE
+
+    
+def main():
+    nums = []
+    READDATAFILE(nums)
+    HEAPSORT(nums)
+
+main()
+
