63747b3b193d7eb60f6e5db66be6899b77fd2210
JhonatanBarrera <JhonatanBarrera>
2016-11-14 14:53:19 -0500

    Uso: CallOrder.bat ubicacionMetodo ubicacionDataSets archivoTiempos

--- MetodosDeOrdenamiento/CallOrder.bat
+++ MetodosDeOrdenamiento/CallOrder.bat
@@ -1,2 +1,2 @@
 @echo off
-FOR /L %%G IN (0,1,9) DO python MetodosDeOrdenamiento\Metodo\Metodo.ext -f MetodosDeOrdenamiento\DataSet\Archivo%%G.txt >> tiemposOrdenamiento.txt
+FOR /L %%G IN (0,1,1) DO python %1 -f %2\Archivo%%G.txt >> %3
