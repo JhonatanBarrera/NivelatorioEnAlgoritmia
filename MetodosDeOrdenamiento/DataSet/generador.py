b37e468373a5c155e5bfb5450feccb3b926d5fcf
Jhonatan Barrera <jhstbarrera@utp.edu.co>
2016-12-06 16:29:37 -0500

    Uso: generador.py -f Archivoi.txt -l cantDatos -u cifraDato

---
+++ MetodosDeOrdenamiento/DataSet/generador.py
@@ -0,0 +1,17 @@
+import argparse
+import random
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="Nombre del archivo a procesar.")
+parser.add_argument("-l", "--length", help="Cantidad de elementos.")
+parser.add_argument("-u", "--units", help="Longitud por elemento.")
+args = parser.parse_args()
+
+with open(args.file, 'w') as file:
+    for i in range(0,int(args.length)):
+        n = random.randint(1, 9)
+        file.write(str(n))
+        for i in range(1,int(args.units)):
+            n = random.randint(0, 9)
+            file.write(str(n))
+        file.write(' ')
